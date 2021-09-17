eb188945a5428522c3ba88eaaacb08bcc6151c0f
Anderson Lucas Azzolini <lucas.azzolini@torfresma.com.br>
2021-09-17 15:51:37 -0300

    corrigido BUG SPLASH PAGE

--- src/index.js
+++ src/index.js
@@ -5,17 +5,21 @@ import Inicio from './pages/inicio';
 import PartidasSalvas from './pages/partidasSalvas';
 import NovaPartida from './pages/novaPartida';
 import PartidaEmAndamento from './pages/partidaEmAndamento';
-
+import { View } from 'react-native'
 const Stack = createNativeStackNavigator();
 export default function App() {
+
   return (
-    <NavigationContainer>{
-      <Stack.Navigator initialRouteName="Início" >
-        <Stack.Screen name="Início" component={Inicio}/>
-        <Stack.Screen name="Partidas Salvas" component={PartidasSalvas} />
-        <Stack.Screen name="Nova Partida" component={NovaPartida} />
-        <Stack.Screen name="Partida em Andamento" component={PartidaEmAndamento} />
-      </Stack.Navigator>
-    }</NavigationContainer>
+    <View style={{ flex: 1, backgroundColor: '#EEEEEE' }}>
+      <NavigationContainer>{
+        <Stack.Navigator initialRouteName="Início"  >
+          <Stack.Screen name="Início" component={Inicio} />
+          <Stack.Screen name="Partidas Salvas" component={PartidasSalvas} />
+          <Stack.Screen name="Nova Partida" component={NovaPartida} />
+          <Stack.Screen name="Partida em Andamento" component={PartidaEmAndamento} />
+        </Stack.Navigator>
+      }</NavigationContainer>
+    </View>
+
   );
 }
